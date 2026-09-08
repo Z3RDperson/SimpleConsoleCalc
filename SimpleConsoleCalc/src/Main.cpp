54a380c0d3f4f1b9745d9eb7e056a23905b793fb
Z3RDperson <fadyz3rdpersonbf@gmail.com>
2026-09-08 16:51:09 +0300

    Revert "Added useless comments to test Git"
    This reverts commit f0f3cadcb0575b8321dad5352efefe587b070980.

--- SimpleConsoleCalc/src/Main.cpp
+++ SimpleConsoleCalc/src/Main.cpp
@@ -1,13 +1,10 @@
-#include <iostream> // Library for outputting data
+#include <iostream>
 
 int main()
 {
-	// Testing and looging hello world to console
 	std::cout << "hello world" << std::endl;
 
-	// Wait for user to press anything so he can see message
 	std::cin.get();
-	// Exit with status 0 if everything fine
 	return 0;
 	//lol
 }
