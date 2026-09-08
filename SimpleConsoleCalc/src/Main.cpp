f0f3cadcb0575b8321dad5352efefe587b070980
Z3RDperson <fadyz3rdpersonbf@gmail.com>
2026-09-08 16:28:58 +0300

    Added useless comments to test Git

--- SimpleConsoleCalc/src/Main.cpp
+++ SimpleConsoleCalc/src/Main.cpp
@@ -1,9 +1,12 @@
-#include <iostream>
+#include <iostream> // Library for outputting data
 
 int main()
 {
+	// Testing and looging hello world to console
 	std::cout << "hello world" << std::endl;
 
+	// Wait for user to press anything so he can see message
 	std::cin.get();
+	// Exit with status 0 if everything fine
 	return 0;
 }
