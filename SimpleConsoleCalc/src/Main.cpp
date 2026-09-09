b027ba766f5bfb1c405c4528e4f177f14c63d05b
Z3RDperson <fadyz3rdpersonbf@gmail.com>
2026-09-09 15:16:54 +0300

    Revert "Added string buffer to take operation needed from user"
    This reverts commit 3ca765880e7c5034b38ed56eab6f2d4696a544b2.

--- SimpleConsoleCalc/src/Main.cpp
+++ SimpleConsoleCalc/src/Main.cpp
@@ -1,14 +1,8 @@
 #include <iostream>
-#include <string>
 
 int main()
 {
-	std::cout << "Type the operation you need\n" 
-		<< "(Multiplication - Division - Difference - Sum) : " << std::endl;
-
-	// String buffer to take input from user
-	std::string op = " ";
-	std::getline(std::cin, op);
+	std::cout << "hello world" << std::endl;
 
 	std::cin.get();
 	return 0;
