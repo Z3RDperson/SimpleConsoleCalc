3ca765880e7c5034b38ed56eab6f2d4696a544b2
Z3RDperson <fadyz3rdpersonbf@gmail.com>
2026-09-09 11:18:23 +0300

    Added string buffer to take operation needed from user

--- SimpleConsoleCalc/src/Main.cpp
+++ SimpleConsoleCalc/src/Main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
-	std::cout << "hello world" << std::endl;
+	std::cout << "Type the operation you need\n" 
+		<< "(Multiplication - Division - Difference - Sum) : " << std::endl;
+
+	// String buffer to take input from user
+	std::string op = " ";
+	std::getline(std::cin, op);
 
 	std::cin.get();
 	return 0;
