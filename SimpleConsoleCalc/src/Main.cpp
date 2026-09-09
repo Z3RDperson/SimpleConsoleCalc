ad9c7fb5ecdb6c6045cb9dc023f62586e900809e
Z3RDperson <fadyz3rdpersonbf@gmail.com>
2026-09-09 15:33:31 +0300

    Storing user prompt in a buffer

--- SimpleConsoleCalc/src/Main.cpp
+++ SimpleConsoleCalc/src/Main.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
+#include <string>
+
+/*
+Header file containing:
+  1- Text-to-operation interpreter
+  2- Operational Structures & Processors
+  3- Any custom classes/structs
+*/
 
 int main()
 {
-	std::cout << "hello world" << std::endl;
+	// Get user input
+	std::cout << "Type a prompt: " << std::endl;
+	std::string buffer;
+	std::getline(std::cin, buffer);
+
+	// Process and parse text
+
+	// Print result
 
 	std::cin.get();
-	return 0;
 }
