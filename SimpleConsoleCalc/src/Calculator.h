3bad6946a45dcf948748f9050015046905be1026
Z3RDperson <fadyz3rdpersonbf@gmail.com>
2026-09-12 00:28:45 +0300

    Defined class Operand

--- SimpleConsoleCalc/src/Calculator.h
+++ SimpleConsoleCalc/src/Calculator.h
@@ -3,3 +3,20 @@
 // Functions Declarations
 
 // Operand class definition
+class Operand
+{
+public:
+	void setNumber(double user_number)
+	{
+		m_number = user_number;
+	}
+
+	double getNumber() const
+	{
+		return m_number;
+	}
+
+private:
+	double m_number;
+	char m_opAfter;
+};
